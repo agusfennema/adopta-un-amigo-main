55da97d2c90ef79cfb57367baeb7ef6d42567d12
Agustina Fennema <agusfennema@gmail.com>
2025-07-03 20:27:39 -0300

    contacto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AnimalesComponent } from './components/animales/animales.component';
 import { AnimalFormComponent } from './components/animal-form/animal-form.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'animales', component: AnimalesComponent },
   { path: 'nuevo', component: AnimalFormComponent },
+  { path: 'contactanos', component: ContactoComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
---
+++ src/app/contacto/contacto.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-contacto',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './contacto.component.html',
+  styleUrl: './contacto.component.css'
+})
+export class ContactoComponent {
+
+}
+
+
