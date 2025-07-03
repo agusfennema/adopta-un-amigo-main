298718c233f495a3860ea498fd88ddac9601a704
NicoGGomez <anelecarg@icloud.com>
2025-07-03 17:46:18 -0300

    subida de archivos importantes

--- src/app/components/animal-form/animal-form.component.ts
+++ src/app/components/animal-form/animal-form.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { AnimalService } from '../../services/animal.service';
+import { Animal } from '../../models/animal.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-animal-form',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './animal-form.component.html',
   styleUrls: ['./animal-form.component.css']
 })
-export class AnimalFormComponent {
 
+export class AnimalFormComponent {
+    animal : Animal = {
+    id: -1,
+    nombre: '',
+    especie: '',
+    edad: 0,
+    fotoUrl: '',
+    descripcion: '',
+    adoptado: false 
+    }
+
+    constructor(private animalService: AnimalService) {}
+
+    agregar() {
+      if (this.animal.nombre) {
+      this.animalService.addAnimal(this.animal).subscribe(res => {
+        console.log('Animal agregado!', res);
+        this.animal = {
+        id: -1,
+        nombre: '',
+        especie: '',
+        edad: 0,
+        fotoUrl: '',
+        descripcion: '',
+        adoptado: false
+      };
+      });
+    }
+    }
+    
 }
